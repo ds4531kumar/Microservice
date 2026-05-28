e0f79a4b0f5b7868aa48e6647a3557a91b7a35f6
Dharmendra Kumar Singh <88071449+ds4531kumar@users.noreply.github.com>
2026-05-28 14:19:05 +0530

    Update GetBasketHandler.cs

--- src/Services/Basket/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket/GetBasket/GetBasketHandler.cs
@@ -9,7 +9,6 @@ public class GetBasketQueryHandler(IBasketRepository repository)
     public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
     {
         var basket = await repository.GetBasket(query.UserName);
-        int a =50;
 
         return new GetBasketResult(basket);
     }
