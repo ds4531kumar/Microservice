c08a48677f9e8d6aba9fa3d0fa0b75891af28901
Dharmendra Kumar Singh <88071449+ds4531kumar@users.noreply.github.com>
2026-05-28 14:07:53 +0530

    Test webhook

--- src/Services/Basket/Basket/GetBasket/GetBasketEndpoints.cs
+++ src/Services/Basket/Basket/GetBasket/GetBasketEndpoints.cs
@@ -13,7 +13,6 @@ public class GetBasketEndpoints : ICarterModule
 
             var respose = result.Adapt<GetBasketResponse>();
             int test = 200;
-            int t=  10.0;
 
             return Results.Ok(respose);
         })
